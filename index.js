97db1ed703b79a334ed9ee46f6251011cd1dfa04
Cedric <c.caudron59@gmail.com>
2020-10-30 17:32:23 +0100

    Add Progress Bar

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+
+
 (async function () {
     const chalk = require('chalk');
     const {Aki} = require('aki-api');
     const {Select} = require('enquirer');
     const aki = new Aki('fr');
+    const progress = require('cli-progress');
 
-    const message = chalk.green('Coucou tout le monde! Je suis beau!');
 
+    const bar1 = new progress.SingleBar({}, progress.Presets.shades_classic);
 
     await aki.start();
 
 
     let myAnswer = 0;
 
+    bar1.start(100,0);
+
     while (aki.progress <= 90) {
+        const bar2 = new progress.SingleBar({}, progress.Presets.shades_classic);
+        bar2.start(100, aki.progress);
 
         const answers = [...aki.answers];
 
@@ -23,9 +30,11 @@
         });
 
 
-        myAnswer = await prompt.run();
 
+        myAnswer = await prompt.run();
         await aki.step(answers.indexOf(myAnswer));
+
+
 
     }
 
@@ -33,5 +42,9 @@
     await aki.win();
     console.log(chalk.green('J\'ai trouvé lui: :', aki.answers[0].name));
     console.log(chalk.green('guessCount:', aki.guessCount));
+
+
+
+
 
 })();
